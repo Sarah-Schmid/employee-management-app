7b6f1da7aaf687ca6e934eeb205982adf9c53897
sarah-schmid <sarah.schmid@gather.app>
2022-11-12 20:41:51 -0700

    added create and update functionality

--- src/App.js
+++ src/App.js
@@ -2,41 +2,69 @@ import "./index.css";
 import Employee from "./components/employee";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "./components/add-employee";
 
 function App() {
   const [role, setRole] = useState("");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Caleb",
       role: "Developer",
       img: "https://images.pexels.com/photos/3789888/pexels-photo-3789888.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id: 2,
       name: "Ellie",
       role: "DevOps",
       img: "https://images.pexels.com/photos/3786525/pexels-photo-3786525.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id: 3,
       name: "Sarah",
       role: "Designer",
       img: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id: 4,
       name: "Michelle",
       role: "Developer",
       img: "https://images.pexels.com/photos/5255233/pexels-photo-5255233.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id: 5,
       name: "Cody",
       role: "Product Manager",
       img: "https://images.pexels.com/photos/3625610/pexels-photo-3625610.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id: 6,
       name: "Mike",
       role: "Project Manager",
       img: "https://images.pexels.com/photos/3777943/pexels-photo-3777943.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -53,14 +81,17 @@ function App() {
             {employees.map((employee) => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
           </div>
+          <AddEmployee newEmployee={newEmployee} />
         </div>
       ) : (
         <p>You do not have access</p>
