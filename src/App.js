3b0bf8d9b5baa0fb523ff0bd335efdddc803a8d0
sarah-schmid <sarah.schmid@gather.app>
2022-11-13 21:39:47 -0700

    moved employee page to /employees using react-router

--- src/App.js
+++ src/App.js
@@ -1,86 +1,20 @@
 import "./index.css";
-import Employee from "./components/employee";
-import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-import AddEmployee from "./components/add-employee";
-import newEmployee from "./components/add-employee";
+import EmployeesPage from "./components/employees-page";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  const [employees, setEmployees] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  function updateEmployee(id, newName, newRole, newImg) {
-    const updatedEmployees = employees.map((employee) => {
-      if (id == employee.id) {
-        return { ...employee, name: newName, role: newRole, img: newImg };
-      }
-      return employee;
-    });
-    setEmployees(updatedEmployees);
-  }
-
-  function newEmployee(name, role, img) {
-    const newEmployee = {
-      id: uuidv4(),
-      name: name,
-      role: role,
-      img: img,
-    };
-    setEmployees([...employees, newEmployee]);
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/employees")
-        .then((res) => {
-          if (!res.ok) {
-            // error coming back from server
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setIsLoading(false);
-          setEmployees(data);
-          setError(null);
-        })
-        .catch((err) => {
-          // auto catches network / connection error
-          setIsLoading(false);
-          setError(err.message);
-        });
-    }, 600);
-  }, []);
-
-  const showEmployees = true;
   return (
-    <div className="App">
-      {showEmployees ? (
-        <div>
-          {isLoading && <div>Loading...</div>}
-          {employees && (
-            <div className="flex flex-wrap justify-center">
-              {employees.map((employee) => {
-                return (
-                  <Employee
-                    key={employee.id}
-                    id={employee.id}
-                    name={employee.name}
-                    role={employee.role}
-                    img={employee.img}
-                    updateEmployee={updateEmployee}
-                  />
-                );
-              })}
-            </div>
-          )}
-          <AddEmployee newEmployee={newEmployee} />
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Switch>
+            <Route path="/employees">
+              <EmployeesPage />
+            </Route>
+          </Switch>
         </div>
-      ) : (
-        <p>You do not have access</p>
-      )}
-    </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/employees-page.js
@@ -0,0 +1,86 @@
+import "../index.css";
+import Employee from "./employee";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "./add-employee";
+
+function EmployeesPage() {
+  const [employees, setEmployees] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  function updateEmployee(id, newName, newRole, newImg) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole, img: newImg };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/employees")
+        .then((res) => {
+          if (!res.ok) {
+            // error coming back from server
+            throw Error("could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setIsLoading(false);
+          setEmployees(data);
+          setError(null);
+        })
+        .catch((err) => {
+          // auto catches network / connection error
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 600);
+  }, []);
+
+  const showEmployees = true;
+  return (
+    <div className="App">
+      {showEmployees ? (
+        <div>
+          {isLoading && <div>Loading...</div>}
+          {employees && (
+            <div className="flex flex-wrap justify-center">
+              {employees.map((employee) => {
+                return (
+                  <Employee
+                    key={employee.id}
+                    id={employee.id}
+                    name={employee.name}
+                    role={employee.role}
+                    img={employee.img}
+                    updateEmployee={updateEmployee}
+                  />
+                );
+              })}
+            </div>
+          )}
+          <AddEmployee newEmployee={newEmployee} />
+        </div>
+      ) : (
+        <p>You do not have access</p>
+      )}
+    </div>
+  );
+}
+
+export default EmployeesPage;
