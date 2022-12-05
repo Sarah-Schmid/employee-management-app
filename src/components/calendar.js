edb294143383ed5b753b227a6f7119ba084750ef
sarah-schmid <sarah.schmid@gather.app>
2022-12-04 21:41:06 -0700

    created unique component out of calendar modal

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -5,10 +5,11 @@ import startOfWeek from "date-fns/startOfWeek";
 import React, { useState, useEffect } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
 import "../index.css";
-import Modal from "react-bootstrap/Modal";
+
+import TimeOffModal from "./calendar-modal";
 
 function Scheduler(props) {
   const locales = {
@@ -22,13 +23,7 @@ function Scheduler(props) {
     locales,
   });
 
-  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
   const [allEvents, setAllEvents] = useState([]);
-
-  //Modal Variables
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   useEffect(() => {
     fetch("https://63545c47ccce2f8c0207b3d7.mockapi.io/api/v1/calendar-data")
@@ -51,77 +46,7 @@ function Scheduler(props) {
   return (
     <div className="Calendar">
       <h1 className="page-title text-purple-700">Time Off Calendar</h1>
-      <div className="center">
-        <button
-          onClick={handleShow}
-          className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
-        >
-          Add Time Off
-        </button>
-      </div>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header className="bg-purple-200" closeButton>
-          <Modal.Title className="text-purple-500">Add Time Off</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="bg-purple-100 rounded">
-          <form
-            onSubmit={(e) => {
-              handleClose();
-              e.preventDefault();
-
-              setAllEvents([...allEvents, newEvent]);
-
-              fetch(
-                "https://63545c47ccce2f8c0207b3d7.mockapi.io/api/v1/calendar-data",
-                {
-                  method: "POST",
-                  headers: { "Content-Type": "application/json" },
-                  body: JSON.stringify(newEvent),
-                }
-              ).then(() => {
-                console.log("new employee added");
-              });
-            }}
-          >
-            <div class="mb-2">
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                placeholder="Employee Name"
-                value={newEvent.title}
-                onChange={(e) =>
-                  setNewEvent({ ...newEvent, title: e.target.value })
-                }
-              />
-            </div>
-            <div class="mb-2">
-              <DatePicker
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholderText="Start Date"
-                style={{ marginRight: "10px" }}
-                selected={newEvent.start}
-                onChange={(start) => setNewEvent({ ...newEvent, start })}
-              />
-            </div>
-            <div class="mb-2">
-              <DatePicker
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholderText="End Date"
-                selected={newEvent.end}
-                onChange={(end) => setNewEvent({ ...newEvent, end })}
-              />
-            </div>
-            <button
-              className="px-4 py-1 text-sm bg-purple-400 text-purple-800 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
-              style={{ marginTop: "10px" }}
-              type="submit"
-            >
-              Add
-            </button>
-          </form>
-        </Modal.Body>
-      </Modal>
+      <TimeOffModal />
       <Calendar
         localizer={localizer}
         events={allEvents}
